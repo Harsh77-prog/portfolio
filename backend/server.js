0153d2b93e7f2ea68f0737070a4c9a9ada2e8c3b
Harsh77-prog <Kumar345@gmail.com>
2025-05-11 10:06:27 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -21,5 +21,5 @@ app.use('/api/questions', questionRoutes);
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on port https://localhost:${PORT}`);
 });
